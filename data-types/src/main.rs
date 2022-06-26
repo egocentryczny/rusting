00656f873c2e53466fa38c5b83c7502f14970d82
egocentryczny <galazka.milosz@gmail.com>
2022-06-26 20:50:12 +0200

    make the extension happy

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    let (x, y, z) = tup;
+    let (_x, y, _z) = tup;
 
     println!("The value of y is: {}", y);
 
     let five_hundred = tup.0;
     println!("The value of five_hund is: {}", five_hundred);
 
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    let b = [3; 5];
+    let _a: [i32; 5] = [1, 2, 3, 4, 5];
+    let _b = [3; 5];
     
 }
